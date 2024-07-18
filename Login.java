c4f8528518b2eaf45edef86b62bad5ed91e5e2ab
helenSpring123 <helen.khiyo@icloud.com>
2024-07-17 19:50:54 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -3,8 +3,8 @@ public class hello{
 public static void main (string [] args){
 
 System.out.printLin("Hello World");
-System.out.printLin("hi helen");
-system.out.printLin("Welcome Helen");
+// System.out.printLin("hi helen");
+// system.out.printLin("Welcome Helen");
 
 
 }
