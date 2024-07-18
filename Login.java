3e158d1165eb787791d276ae96b7bc1ef60fd8ef
helenSpring123 <helen.khiyo@icloud.com>
2024-07-17 20:16:54 -0500

    new line

--- Login.java
+++ Login.java
@@ -3,8 +3,13 @@ public class hello{
 public static void main (string [] args){
 
 System.out.printLin("Hello World");
+<<<<<<< HEAD
 // System.out.printLin("hi helen");
 // system.out.printLin("Welcome Helen");
+=======
+System.out.printLin("hi helen");
+
+>>>>>>> parent of d998600 (Update Login.java)
 
 
 }
